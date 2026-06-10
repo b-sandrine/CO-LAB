7f06296513d67ad9da33ac6294c2db21cbb100c1
Dushimimana Sandrine <sanrdushimimana@gmail.com>
2026-06-10 10:52:22 +0200

    Final commit

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,20 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'core/theme/app_theme.dart';
 import 'core/router/app_router.dart';
 import 'services/fcm_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // sqflite_common_ffi required on Windows and Linux desktops
+  if (Platform.isWindows || Platform.isLinux) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
 
   await FcmService.initialize();
 
